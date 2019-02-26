21801d359222a633688ffb8f62157acd38e7b7b5
mac <toshi.import@gmail.com>
2019-02-26 16:45:24 +0900

    add: とりあえずチャート表示完了

--- app/assets/javascripts/chart_sample.js
+++ app/assets/javascripts/chart_sample.js
@@ -1,35 +1,67 @@
+// https://misc.0o0o.org/chartjs-doc-ja/
 window.draw_graph = function () {
-  var barNum, bdColors, bgColors, ctx, i, j, labels, myChart, ref;
   ctx = document.getElementById("myChart").getContext('2d');
-  barNum = 6;
-  labels = new Array(barNum);
-  bgColors = new Array(barNum);
-  bdColors = new Array(barNum);
-  for (i = j = 0, ref = barNum; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
-    labels[i] = 'data' + i;
-    bgColors[i] = 'rgba(75, 192, 192, 0.2)';
-    bdColors[i] = 'rgba(75, 192, 192, 1)';
-  }
   return myChart = new Chart(ctx, {
+    // グラフの種類を選ぶ
     type: 'bar',
     data: {
-      labels: labels,
+      // x軸のデータ(日時など)
+      // グラフが1つのみであればここにlabelプロパティを記述
+      labels: gon.labels,
       datasets: [
+        // 1つめのデータ
         {
-          label: '# of Votes',
-          data: gon.data,
-          backgroundColor: bgColors,
-          borderColor: bdColors,
-          borderWidth: 1
+          // 対応するグラフの説明
+          label: '売上',
+          // 実際に使うデータ（配列で渡す）
+          data: gon.sales,
+          // グラフの色の指定
+          backgroundColor: 'rgba(75, 192, 192, 1)',
+          // グラフ外枠の色指定
+          borderColor: 'rgba(75, 192, 192, 1)',
+          // グラフの外枠の幅指定
+          borderWidth: 1,
+          // 線の下の領域を色付けの可否(true, false)
+          fill: false,
+          // 線グラフの曲線
+          lineTension: 0,
+          // 点の半径(px)
+          pointRadius: 5,
+          // 点の図形(defaultはcircle)
+          pointStyle: 'triangle',
+          // マウスオーバー検出の範囲(px)
+          pointHitRadius: 5,
+          yAxesID: "Yの左軸"
+        },
+        // 2つめのデータ
+        {
+          // 2つめのデータのグラフの種類を選ぶ
+          type: 'line',
+          label: 'ユーザー登録数',
+          data: gon.sign_up_users,
+          yAxesID: "Yの右軸"
         }
-      ]
+      ],
+      // グラフが2つであればここにlabelプロパティを記述
+      // labels: gon.labels,
     },
     options: {
       scales: {
+        xAxes:[{}],
         yAxes: [
           {
+            id: "Yの左軸",
+          },
+          {
+            id: "Yの右軸",
+            position: "right",
+            scaleLabel:{
+              display: true
+            },
             ticks: {
-              beginAtZero: true
+              beginAtZero: true,
+              max: 20,
+              stepSize: 3.0
             }
           }
         ]
--- app/controllers/chart_sample_controller.rb
+++ app/controllers/chart_sample_controller.rb
@@ -1,7 +1,52 @@
 class ChartSampleController < ApplicationController
   def index
+    t = Time.current
+    this_year = t.year
+    this_month = t.month
+    this_month_days = t.end_of_month.day
+    today = t.day
+    day = 24 * 60 * 60
+    youbi = %w[日 月 火 水 木 金 土]
+
+    # label of x-axis
+    gon.labels = []
+    this_month_days.times do |i|
+      gon.labels << "#{i + 1}(#{youbi[(t.beginning_of_month + i * day).wday]})"
+    end
+    # orders = Order.select("date(created_at) as ordered_date, sum(price) as total_price").group("date(created_at)").where('created_at >= ?', t.beginning_of_month)
+    # @orders = Order.includes([:user, :address, cartitems: :item]).where('created_at >= ?', t.beginning_of_month)
+    @orders = Order.where('created_at >= ?', t.beginning_of_month)
+    @sales_per_day = @orders.group("DAY(created_at)").sum(:amount)
+    @signup_users = User.where('created_at >= ?', t.beginning_of_month)
+    @users_per_day = @signup_users.group("DAY(created_at)").count(:id)
+    # @users_per_day = @orders.group("DAY(created_at)").count(:user_id)
+
+    gon.sales = []
+    this_month_days.times do |i|
+      if @sales_per_day[i+1] == nil
+        gon.sales << 0
+      else
+        gon.sales << @sales_per_day[i+1]
+      end
+      # @sales << @sales_per_day[i+1] == nil? ? 0 : @sales_per_day[i+1]
+      # @sales << @sales_per_day[i+1] || 0
+    end
+
+    gon.sign_up_users = []
+    this_month_days.times do |i|
+      if @users_per_day[i+1] == nil
+        gon.sign_up_users << 0
+      else
+        gon.sign_up_users << @users_per_day[i+1]
+      end
+    end
+
+
+
+    # binding.pry
+
     gon.data = []
-    6.times do
+    this_month_days.times do
       gon.data << rand(100.0)
     end
   end
