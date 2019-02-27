2371d5761f9942f720fc759e26fc380253764855
mac <toshi.import@gmail.com>
2019-02-27 21:23:58 +0900

    update: 不要部分の編集

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  # bullet settings
+  config.after_initialize do
+    Bullet.enable = true # Bulletプラグインを有効
+    Bullet.alert = true # JavaScriptでの通知
+    Bullet.bullet_logger = true # log/bullet.logへの出力
+    Bullet.console = true # ブラウザのコンソールログに記録
+    Bullet.rails_logger = true # Railsログに出力
+  end
 end
